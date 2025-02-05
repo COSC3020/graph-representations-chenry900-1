77aa38d1cdeb344efae68653c333226b7bca2664
chenry900 <158115154+chenry900@users.noreply.github.com>
2025-02-05 15:49:43 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+    var adjList = [];
+
+    for(var o = 0; o < adjMatrix.length; o++) {// o for outside not zero
+        adjList[o] = [];
+        for (var i = 0; i < adjMatrix.length; i++) {
+            if (adjMatrix[o][i] == 1) {
+                adjList[o].push(i);
+            }
+        }
+    }        
+    return adjList;
 }
